fee31cca23069f7fbaa735d2e4c905f3d083f811
wtfil <evgen.filatov@gmail.com>
2014-07-19 09:52:32 +0300

    added ino file

---
+++ arduino-bluetooth.ino
@@ -0,0 +1,22 @@
+int LED_PIN = 6;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print('AT');
+  Serial.print('AT+PIN1234');
+  pinMode(LED_PIN, OUTPUT);
+}
+
+
+void loop() {
+  if (Serial.available() > 0){
+  
+    switch (Serial.read()) {
+      case '0': digitalWrite(LED_PIN, LOW); break;
+      case '1': digitalWrite(LED_PIN, HIGH); break;
+    }
+   
+  }
+ 
+  delay(200); 
+}
